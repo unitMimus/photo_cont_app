c80edeeffe234239acf833991d081a31bfefccee
unitMimus <stfyodorov@gmail.com>
2019-01-21 14:02:38 +0300

    simple tests for static pages, test guard, routing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test "should get root" do
+    get root_path
+    assert_select "p", count: 2
+#    assert_select "title", "PhotoContApp"
+    assert_select "h1", "home"
     assert_response :success
   end
 
+=begin
+    test "should get home" do
+      get home_path
+      assert_response :success
+    end
+=end
+
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
   end
 
